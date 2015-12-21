b6cf8d27271d26a6c0c0aaf4d781a70f65e5ba9f
Albert Li <albert_lee@hotmail.com>
2015-12-21 10:45:36 -0800

    no message

--- addon/components/pan-zoom.js
+++ addon/components/pan-zoom.js
@@ -3,26 +3,52 @@ import layout from '../templates/components/pan-zoom';
 
 export default Ember.Component.extend({
   layout: layout,
-  eventNamespace: ".panzoom",
-  transition: true,
-  cursor: "move",
-  disablePan: false,
-  disableZoom: false,
-  increment: 0.3,
-  minScale: 0.4,
-  maxScale: 5,
-  rangeStep: 0.05,
-  duration: 200,
-  easing: "ease-in-out",
-  contain: false,
-  startTransform: undefined,
+  zooming: false,
+  // options which match jquery.panzoom options.
+  options: {
+    eventNamespace: ".panzoom",
+    transition: true,
+    cursor: "move",
+    disablePan: false,
+    disableZoom: false,
+    increment: 0.3,
+    minScale: 0.4,
+    maxScale: 5,
+    rangeStep: 0.05,
+    duration: 200,
+    easing: "ease-in-out",
+    contain: false,
+    startTransform: undefined,
+  },
   
-  onZoomChanged: Ember.observer('zooming', function() {
+  // computed alias for property
+  eventNamespace: Ember.computed.alias("options.eventNamespace"),
+  transition: Ember.computed.alias("options.transition"),
+  cursor: Ember.computed.alias("options.eventNamespace"),
+  disablePan: Ember.computed.alias("options.cursor"),
+  disableZoom: Ember.computed.alias("options.disableZoom"),
+  increment: Ember.computed.alias("options.increment"),
+  minScale: Ember.computed.alias("options.minScale"),
+  maxScale: Ember.computed.alias("options.maxScale"),
+  rangeStep: Ember.computed.alias("options.rangeStep"),
+  duration: Ember.computed.alias("options.duration"),
+  easing: Ember.computed.alias("options.easing"),
+  contain: Ember.computed.alias("options.contain"),
+  startTransform: Ember.computed.alias("options.startTransform"),
+  
+  onZoomChanged: Ember.observer("zooming", function() {
     let zoomOut = this.get("zooming")
     this.get("$panzoom").panzoom('zoom', zoomOut, {
-      increment: 0.1,
+      increment: this.get("increment"),
       animate: false,
     });
+  }),
+  
+  /*
+   * @method observer option change, and set panzoom option
+   */
+  onOptionChanged: Ember.observer("options", function() {
+    this.get("$panzoom")("options", this.get("options"));
   }),
   
   didInsertElement() {

6334250289d3b60d07937a2f7fa1fbbc47272511
Ben Schuiling <benjaminschuiling@smarttech.com>
2017-07-03 14:58:11 -0700

    Updating to panzoom 3.3.2 with bugfix. Integrating with mousewheel directly, migrating to Ember-CLI 2.13.3, moving off of bower and onto npm

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /* jshint node: true */
 'use strict';
 var path = require('path');
+const nodeModulesPath = require('node-modules-path');
 
 module.exports = {
   name: 'ember-cli-panzoom',
-  
+
   blueprintsPath: function() {
     return path.join(__dirname, 'blueprints');
   },
@@ -12,6 +13,8 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
     this.app.import("vendor/ie-touch.js");
-    this.app.import(app.bowerDirectory + '/jquery.panzoom/dist/jquery.panzoom.js');
+
+    this.app.import(this.project.nodeModulesPath + '/jquery-mousewheel/jquery.mousewheel.js');
+    this.app.import(this.project.nodeModulesPath + '/jquery.panzoom/dist/jquery.panzoom.min.js');
   }
 };

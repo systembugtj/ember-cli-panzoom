25428a759195527d72e2b6c1ae51e103a87d3ddb
Ben Schuiling <benjaminschuiling@smarttech.com>
2017-08-14 14:03:48 -0700

    Moving the files to get imported from vendor

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* jshint node: true */
 'use strict';
-var path = require('path');
+let path = require('path');
+let mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-cli-panzoom',
@@ -9,11 +10,20 @@ module.exports = {
     return path.join(__dirname, 'blueprints');
   },
 
+  treeForVendor: function(tree) {
+    let jqueryMousewheel = path.join(this.project.nodeModulesPath, 'jquery-mousewheel');
+    let jqueryPanzoom = path.join(this.project.nodeModulesPath, 'jquery.panzoom/dist');
+
+    tree = (tree) ? mergeTrees([ tree, jqueryMousewheel, jqueryPanzoom]) : mergeTrees([jqueryMousewheel, jqueryPanzoom]);
+
+    return tree;
+  },
+
   included: function(app) {
     this._super.included(app);
     this.app.import("vendor/ie-touch.js");
 
-    this.app.import(this.project.nodeModulesPath + '/jquery-mousewheel/jquery.mousewheel.js');
-    this.app.import(this.project.nodeModulesPath + '/jquery.panzoom/dist/jquery.panzoom.min.js');
+    this.app.import('vendor/jquery.mousewheel.js');
+    this.app.import('vendor/jquery.panzoom.js');
   }
 };
